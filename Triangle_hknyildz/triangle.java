4867e08004b801aff4ad4079d5d93f4c0adb3030
Hakan Yıldız <68272945+hknyildz@users.noreply.github.com>
2021-08-09 17:20:24 +0300

    CH06_35 Triangle_hknyildz

---
+++ Triangle_hknyildz/triangle.java
@@ -0,0 +1,19 @@
+package homework.Triangle;
+
+public class triangle {
+	public static void main(String[] args) {
+		int t=9;
+		for(int i=0;i<10;i++)
+		{
+			
+			for( int k=0;k<t;k++) 
+			{
+				System.out.print("*");
+			}
+			t--;
+			System.out.println("*");
+		}
+	}
+	
+
+}
